d6aadb3c83c94820f9da4a21647587451b67d79f
Nisha Pokharel <nishapokharel2018@gmail.com>
2023-09-24 22:20:18 +0545

    DAY 1 OF MERN

---
+++ Demo JS/script.js
@@ -0,0 +1,20 @@
+const name = "Nisha";
+const name1 = "Garima";
+
+console.log(name);
+console.log(name1);
+
+// Array
+
+const names = ["Nisha", "Garima", "Anisha"];
+console.log(names);
+console.log(names.length); // Use names.length to get the length of the array
+
+// Objects
+const person = {
+  name: "Nisha", // Enclose property names in quotes
+  age: 21,
+  Profession: "Student"
+};
+console.log(person);
+alert("hello world")
---
+++ learningnode/app.js
@@ -0,0 +1,23 @@
+console.log("Hello World")
+console.log(1+2)
+
+const a = 10;
+const b = 20;
+console.log(a)
+console.log(b)
+
+const sum = a+b
+
+console.log(sum)
+
+// array
+const number = [5,10,15,20]
+console.log(number[0])
+console.log(number.length)
+
+const person = {
+
+    name : "Nisha",
+    Age : "22"
+}
+console.log(person.name)
